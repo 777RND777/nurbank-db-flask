2fe281b4c642a4f376e1fd2b5c9077841987a97d
Nurmukhanbet Rakhimbayev <777rnd777@gmail.com>
2022-02-28 19:42:42 +0600

    same thread warnings were solved

--- bank/database.py
+++ bank/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///bank/db.sqlite"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,
+                       connect_args={"check_same_thread": False})
 session = scoped_session(sessionmaker(autocommit=False,
                                       autoflush=False,
                                       bind=engine))
