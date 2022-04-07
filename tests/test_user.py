3140a3fc9a3e22e2921757662a80d76ab7b8747e
Nurmukhanbet Rakhimbayev <777rnd777@gmail.com>
2022-04-07 21:05:08 +0600

    empty checks in user tests were filled

--- tests/test_user.py
+++ tests/test_user.py
@@ -17,7 +17,7 @@ def test_get_user(client, user):
     assert response.status_code == 200
 
 
-def test_get_user_applications_empty(client, user):
+def test_get_user_applications(client, user, application):
     response = client.get(f"/users/{user['_id']}/applications")
     assert response.status_code == 404
 
@@ -28,13 +28,37 @@ def test_get_user_applications_empty(client, user):
     assert response.status_code == 200
     assert len(response.json) == 0
 
+    response = client.post("/applications", json=application)
+    assert response.status_code == 200
+    response = client.post("/applications", json=application)
+    assert response.status_code == 200
+    response = client.post("/applications", json=application)
+    assert response.status_code == 200
 
-def test_get_user_pending_empty(client, user):
+    response = client.get(f"/users/{user['_id']}/applications")
+    assert response.status_code == 200
+    assert len(response.json) == 3
+
+
+def test_get_user_pending(client, user, application, auth):
     response = client.get(f"/users/{user['_id']}/pending")
     assert response.status_code == 404
 
     response = client.post("/users", json=user)
     assert response.status_code == 200
+
+    response = client.get(f"/users/{user['_id']}/pending")
+    assert response.status_code == 204
+
+    response = client.post("/applications", json=application)
+    assert response.status_code == 200
+    application_id = response.json['_id']
+
+    response = client.get(f"/users/{user['_id']}/pending")
+    assert response.status_code == 200
+
+    response = client.put(f"/applications/{application_id}/decline", json=auth)
+    assert response.status_code == 201
 
     response = client.get(f"/users/{user['_id']}/pending")
     assert response.status_code == 204
